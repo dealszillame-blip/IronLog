5e30ba39f4f5b67ac6f811356710a64e0e76230c
dealszillame-blip <dealszillame@gmail.com>
2026-03-04 02:22:21 +1100

    Add initial project files for IronLog based on user request on 2026-03-03 15:22:07 UTC.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Gerrard from './gerrard';
+
+function App() {
+  return (
+    <div>
+      <h1>IronLog Gerrard Demo</h1>
+      <Gerrard />
+    </div>
+  );
+}
+
+export default App;
